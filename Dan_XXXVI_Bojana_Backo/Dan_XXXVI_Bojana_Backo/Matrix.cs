de0546aab60b77975d19de30a3cb734ba2d6e784
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-06-22 15:32:56 +0200

    Add Matrix.cs Program.cs

---
+++ Dan_XXXVI_Bojana_Backo/Dan_XXXVI_Bojana_Backo/Matrix.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace Dan_XXXVI_Bojana_Backo
+{
+    public class Matrix
+    {
+        readonly object listLock = new object();
+        static List<int> list = new List<int>();
+        Random random = new Random();
+
+        // Function for creating the matrix of range 100x100
+        public void EmptyToFullMatrix()
+        {
+            int[,] matrix = new int[100, 100];
+            // Lock the critical section of code
+            lock (listLock)
+            {
+                while (list.Count != 10000)
+                {
+                    // Wait while list count is different from max 10000
+                    Monitor.Wait(listLock);
+                }
+                Console.WriteLine(Thread.CurrentThread.Name + " is filling the matrix.");
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = list[i + j];
+                    }
+                }
+            }
+        }
+        // Function for genetaring random numbers
+        public void GenerateNumbers()
+        {
+            // Lock the critical section of code
+            lock (listLock)
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    int number = random.Next(10, 100);
+                    list.Add(number);
+                }
+                // When the 10000th random number is generated Puls to
+                // the EmptyToFullMatrix() function to fill the matrix
+                Monitor.Pulse(listLock);
+                Console.WriteLine(Thread.CurrentThread.Name + " generated numbers.");
+            }
+        }
+
+        public void WriteOddNumbersToFile()
+        {
+            List<int> oddNumbers = new List<int>();
+            foreach (var item in list)
+            {
+                if (item % 2 != 0)
+                {
+                    oddNumbers.Add(item);
+                }
+            }
+            try
+            {
+                File.Delete(Program.fileOddNumbers);
+                using (StreamWriter sw = File.CreateText(Program.fileOddNumbers))
+                {
+                    foreach (var item in oddNumbers)
+                    {
+                        sw.WriteLine(item);
+                    }
+                    sw.Close();
+                    Console.WriteLine(Thread.CurrentThread.Name + " has finished writing to the file!");
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file was not found: '{e}'");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The file could not be opened: '{e}'");
+            }
+        }
+
+        public void ReadOddNumbersFromFile()
+        {
+            try
+            {
+                using (StreamReader sr = File.OpenText(Program.fileOddNumbers))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.Write(line);
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"The file was not found: '{e}'");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"The file could not be opened: '{e}'");
+            }
+        }
+    }
+}
--- Dan_XXXVI_Bojana_Backo/Dan_XXXVI_Bojana_Backo/Program.cs
+++ Dan_XXXVI_Bojana_Backo/Dan_XXXVI_Bojana_Backo/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Dan_XXXVI_Bojana_Backo
 {
     class Program
     {
+        public static string fileOddNumbers = @"..\..\FileByThread.txt";
+
         static void Main(string[] args)
         {
+            Matrix matrix = new Matrix();
+            Thread t1 = new Thread(() => matrix.EmptyToFullMatrix());
+            t1.Name = "First Thread";
+            t1.Start();
+            Thread t2 = new Thread(() => matrix.GenerateNumbers());
+            t2.Name = "Second Thread";
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            Thread t3 = new Thread(() => matrix.WriteOddNumbersToFile());
+            t3.Name = "Third Thread";
+            Thread t4 = new Thread(() => matrix.ReadOddNumbersFromFile());
+            t4.Name = "Fourth Thread";
+            t3.Start();
+            t3.Join();
+            t4.Start();
+            t4.Join();
+            Console.ReadLine();
         }
+
+        
     }
 }
